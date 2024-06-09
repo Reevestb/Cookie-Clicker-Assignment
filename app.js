6234180b6a12800e826f866c8a26e4b3e87d2e26
Theo Reeves <theo_25@icloud.com>
2024-06-09 19:51:00 +0100

    added and recieved local storage, made shop items buyable, media Queries and css styling

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const shopUpgrades = document.getElementById("shop");
 
 //A way to store the shop items that we get from the API
 let shopItems;
-
 //Fetch the items from the API (async, await)
 async function getShopItems() {
   //1. fetch shop items from api
@@ -21,12 +20,12 @@ async function getShopItems() {
   //2. turn data into JSON --> .json()
   const data = await response.json();
   console.log(data);
-  //3. push the items in the shop items array
   return data;
 }
 
 //creating an async function to be able to access the array of objects.
 async function createShopItems() {
+  reloadPage();
   let fetchedItems = await getShopItems();
   namingShopItems(fetchedItems);
 }
@@ -34,41 +33,32 @@ createShopItems();
 
 function namingShopItems(shopArray) {
   shopArray.forEach((itemOfShop) => {
-    const upgradeName = document.createElement("h1");
+    const upgradeName = document.createElement("h2");
     const upgradeCost = document.createElement("p");
     const upgradeIncrease = document.createElement("p");
     const upgradeShopBtn = document.createElement("button");
-    upgradeShopBtn.innerText = `${itemOfShop.cost} buy`;
+    upgradeShopBtn.innerText = `${itemOfShop.cost} Buy`;
     upgradeName.textContent = itemOfShop.name;
     upgradeCost.textContent = itemOfShop.cost;
     upgradeIncrease.textContent = `${itemOfShop.increase} Cookies Per Second`;
     shopUpgrades.append(upgradeName, upgradeIncrease, upgradeShopBtn);
     upgradeShopBtn.addEventListener("click", () => {
-      cps = cps + itemOfShop.increase;
+      if (cookiesOwned >= itemOfShop.cost) {
+        cookiesOwned -= itemOfShop.cost;
+        cps = cps + itemOfShop.increase;
+        cpsBox.innerText = `${cps} Cookies Per Second`;
+      }
     });
   });
 }
-
-// function namingShopItems(itemOfShop) {
-//     const name = document.createElement("h1");
-//     const cost = document.createElement("p");
-//     const increase = document.createElement("p");
-//     name.textContent = itemOfShop[0].name;
-//     cost.textContent = itemOfShop[0].cost;
-//     increase.textContent = itemOfShop[0].increase;
-//     console.log(name);
-//     console.log(cost);
-//     shopUpgrades.append(name, cost, increase);
-
-//   }
-
 //I need an addEventListener() to clicking our cookie
-const clickButton = document.getElementById("startButton");
 //select the cookie img or button
 //write an event listener
+const clickButton = document.getElementById("startButton");
 clickButton.addEventListener("click", function () {
   cookiesOwned++;
   totalOwnedBox.innerText = `${cookiesOwned} Cookies`;
+  saveLocalStorage();
 });
 //when i click the button, the value of cookiesOwned goes up by one
 //increment operator
@@ -76,10 +66,9 @@ const resetButton = document.getElementById("Reset");
 resetButton.addEventListener("click", function () {
   cookiesOwned = 0;
   cps = 1;
-  saveLocalStorage();
+  totalOwnedBox.textContent = 0;
+  cpsBox.textContent = `${cps} Cookies Per Second`;
 });
-//We need to have all the game information in one function
-//you need to check if there is any values stored in local storage. (cookiesOwned and cps (cookie per second)).
 
 //we need a timer to increase the cps // setInterval() timer
 setInterval(function () {
@@ -89,17 +78,19 @@ setInterval(function () {
 }, 1000);
 cpsBox.textContent = `${cps} Cookies Per Second`;
 
+//you need to check if there is any values stored in local storage. (cookiesOwned and cps (cookie per second)).
+
 function saveLocalStorage() {
   localStorage.setItem("cookiesOwned", cookiesOwned);
   localStorage.setItem("cps", cps);
 }
 
-let storedCookOwned = localStorage.getItem("cookiesOwned");
-let storedCps = localStorage.getItem("cps");
-
 function reloadPage() {
-  let parsedCookies = json.parse(storedCookOwned);
-  let parsedCps = json.parse(storedCps);
+  let storedCookOwned = localStorage.getItem("cookiesOwned");
+  let storedCps = localStorage.getItem("cps");
+  let parsedCookies = JSON.parse(storedCookOwned);
+  let parsedCps = JSON.parse(storedCps);
+
   if (localStorage.getItem("cps")) {
     cps = parsedCps;
   }
@@ -107,25 +98,3 @@ function reloadPage() {
     cookiesOwned = parsedCookies;
   }
 }
-
-//increase the cookiesOwned by one every second
-//I want to update the value displayed on the page (or you could have this in a seperate function that you call inside the interval, for example updateDisplay() )
-//I want to update the value in local storage (or you could have this in a seperate function that you call inside the interval, for example saveLocalStorage())
-
-//extra tools, if you want to use them to seperate different tasks into functions.
-
-// function updateDisplay() {
-//update the DOM element containing the value of cookiesOwned
-//update the content value of the cookies from local storage  (current total)
-//}
-
-// function saveLocalStorage() {
-// a method to turn your data into strings
-//a method to set the items using key and value in local storage
-//}
-
-// function rederShop() {
-//create DOM elements to display your shop items
-//you can use for loop or an array method
-//shopItems.forEach(() => {});
-//}
